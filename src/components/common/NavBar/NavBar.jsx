e0c01431ea5ae486e3898de5695c5281842c54ea
Gregory0401 <grigoriosdart@gmail.com>
2022-09-19 23:36:59 +0300

    changed path for nav bar

--- src/components/common/NavBar/NavBar.jsx
+++ src/components/common/NavBar/NavBar.jsx
@@ -12,12 +12,12 @@ export default function NavBar() {
       <Container>
         <div style={{display: 'flex', justifyContent: 'space-between'}}>
           <nav className={s.nav}>
-            <NavLink className={s.link} to='/'>Home</NavLink>
-            <NavLink className={s.link} to='/movies'>Movies</NavLink>
+            <NavLink className={s.link} to='/home/'>Home</NavLink>
+            <NavLink className={s.link} to='/home/movies'>Movies</NavLink>
           </nav>
           <Routes>
-            <Route path='/' element={false}/>
-            <Route path='/movies' element={
+            <Route path='/home/' element={false}/>
+            <Route path='/home/movies' element={
               <div className={s.stat}>Results: {moviesCount}/{totalResults}</div>
             } />
             <Route path='*' element={false} />
